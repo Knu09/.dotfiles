c45f7e74131d9d44770a2c813eb008f25923851a
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-04-20 01:39:49 +0800

    delete: require files for lsp

--- .config/nvim/lua/plugins/lsp/emmet.lua
+++
@@ -1,6 +0,0 @@
-local util = require("lspconfig/util")
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-return {}
