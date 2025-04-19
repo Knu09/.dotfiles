07d6e78cd93d2eb3a194289b8fcb48a78bca5879
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-04-19 21:27:08 +0800

    update: options.lua

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,3 +5,15 @@
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+vim.wo.number = true
+vim.o.relativenumber = true
+vim.o.linebreak = true
+vim.o.autoindent = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
+vim.o.wrap = true
