bcf1426fdb74a52e6865dc0898daaaf429903a54
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-05-01 12:06:35 +0800

    update: options.lua

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,7 +1,3 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
@@ -10,10 +6,18 @@ vim.wo.number = true
 vim.o.relativenumber = true
 vim.o.linebreak = true
 vim.o.autoindent = true
+vim.o.smartindent = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
+
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.expandtab = true
+
+vim.o.scrolloff = 10
 vim.o.wrap = true
+vim.o.breakindent = true
+vim.opt.backspace = { "start", "eol", "indent" }
+vim.opt.path:append({ "**" })
+vim.opt.wildignore:append({ "*/node_modules/*" })
