5d15a77f924bec121fd594cf0b24192e509d2699
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-06-14 19:55:54 +0800

    Add: todo-comments nvim

---
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -0,0 +1,6 @@
+return {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {}, -- leave empty or configure as needed
+  event = "VeryLazy", -- optional: defer loading
+}
