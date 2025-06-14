8b092de61fbc86bb13fb357b82af2797fda0c46b
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-06-14 20:00:17 +0800

    Update: configuration signs to false

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -1,6 +1,8 @@
 return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {}, -- leave empty or configure as needed
+  opts = {
+    signs = false,
+  },
   event = "VeryLazy", -- optional: defer loading
 }
