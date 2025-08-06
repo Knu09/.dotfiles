f4751fbcc6fa70015d7e8f2b1dfb8b4feb537725
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-08-06 23:17:29 +0800

    update (treesitter.lua): insert java in ensured installed

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -6,6 +6,7 @@ return {
       local config = require("nvim-treesitter.configs")
       config.setup({
         ensure_installed = {
+          "java",
           "c",
           "rust",
           "go",
