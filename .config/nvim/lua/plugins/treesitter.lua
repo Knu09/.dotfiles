7506fbbed7dbaa637239b33deefbb38522979d63
Knu09 <132640979+Knu09@users.noreply.github.com>
2026-01-17 17:50:25 +0800

    refactor (treesitter): disabled treesitter context

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -52,7 +52,7 @@ return {
     "nvim-treesitter/nvim-treesitter-context",
     config = function()
       require("treesitter-context").setup({
-        enable = true,
+        enable = false,
         max_line = 3,
         trim_scope = "outer",
         mode = "cursor",
