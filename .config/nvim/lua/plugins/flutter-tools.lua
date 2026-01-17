c3f591f532760fb45f63d43c799b9231ad667da0
Knu09 <132640979+Knu09@users.noreply.github.com>
2026-01-17 17:59:14 +0800

    feat (flutter-tools): add keymaps for flutter cmdline

--- .config/nvim/lua/plugins/flutter-tools.lua
+++ .config/nvim/lua/plugins/flutter-tools.lua
@@ -1,3 +1,5 @@
+local telescope = require("telescope")
+
 return {
   {
     "akinsho/flutter-tools.nvim",
@@ -11,6 +13,18 @@ return {
         closing_tags = { enabled = true },
         lsp = { settings = { showTodos = true } },
       })
+      -- Telescope Flutter commands
+      vim.keymap.set("n", "<leader>Ff", function()
+        telescope.extensions.flutter.commands()
+      end, { desc = "Flutter: Telescope Commands" })
+
+      -- Direct Flutter commands
+      vim.keymap.set("n", "<leader>Fr", "<cmd>FlutterReload<CR>", { desc = "Flutter: Hot Reload" })
+      vim.keymap.set("n", "<leader>FR", "<cmd>FlutterRestart<CR>", { desc = "Flutter: Hot Restart" })
+      vim.keymap.set("n", "<leader>Fd", "<cmd>FlutterDevices<CR>", { desc = "Flutter: List Devices" })
+      vim.keymap.set("n", "<leader>Fe", "<cmd>FlutterEmulators<CR>", { desc = "Flutter: List Emulators" })
+      vim.keymap.set("n", "<leader>Fl", "<cmd>FlutterLogToggle<CR>", { desc = "Flutter: Toggle Dev Log" })
+      vim.keymap.set("n", "<leader>Fc", "<cmd>FlutterLogClear<CR>", { desc = "Flutter: Clear Dev Log" })
     end,
   },
 }
