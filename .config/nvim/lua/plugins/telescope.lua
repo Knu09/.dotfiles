5e2ee45fa2b75eaab8cc99cbf9896790035ce954
Knu09 <132640979+Knu09@users.noreply.github.com>
2026-02-05 11:29:07 +0800

    fix (telescope.lua): Patch: bypass broken dartls capability detection

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -7,18 +7,30 @@ return {
     tag = "0.1.5",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      require("telescope").setup({
+      local telescope = require("telescope")
+      telescope.setup({
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
           },
         },
       })
+
       local builtin = require("telescope.builtin")
+
+      -- Patch: bypass broken dartls capability detection
+      local orig_lsp_definitions = builtin.lsp_definitions
+      builtin.lsp_definitions = function(opts)
+        -- If native LSP works, use it directly
+        vim.lsp.buf.definition()
+      end
+
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
-      require("telescope").load_extension("ui-select")
+      vim.keymap.set("n", "gd", builtin.lsp_definitions, { desc = "Go to Definition (Dart-safe)" })
+
+      telescope.load_extension("ui-select")
     end,
   },
 }
